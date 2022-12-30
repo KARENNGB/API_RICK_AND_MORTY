84245209a6f33eb97a094a750c949e52aa164d0e
KARENNGB <karengutierrezb@hotmail.com>
2022-12-30 09:51:11 +0000

    problems solved

--- src/js/views/details.js
+++ src/js/views/details.js
@@ -1,16 +1,35 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 
-export const Details = props => {
-	const { store, actions } = useContext(Context);
-	const params = useParams();
-	return (
-		<h1>Details</h1>
-	);
+export const Details = () => {
+  const params = useParams();
+  const { store, actions } = useContext(Context);
+
+  useEffect(() => {
+    actions.getSingleCharacter(params.id);
+  }, []);
+
+  return (
+    <div className="col">
+    <div className="horizontal-section" key={store.character.id}>
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">{store.character.name}</h5>
+            <p className="card-text1">
+              <strong>Status: </strong>
+              {store.character.status}
+            </p>
+            <Link to={`/`}>Home</Link>
+            <br></br>
+          </div>
+        </div>
+      </div>
+      </div>
+  );
 };
 
 Details.propTypes = {
-	match: PropTypes.object
+  match: PropTypes.object,
 };
